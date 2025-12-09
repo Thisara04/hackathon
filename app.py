2445d9f084d3b89cab17137b1831d959fce1e443
Thisara Adhikari <140851451+Thisara04@users.noreply.github.com>
2025-12-09 07:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ def fetch_exchange_rates():
         resp = requests.get(url).json()
 
         rates = resp.get("rates", {})
+        print("RAW API DATA:", resp)
         if not rates:
             return {}
 
